ad41b529eaa77a788364f7a01617ef46991df821
AbylovErmek <gskro705@gmail.com>
2021-03-13 05:55:09 +0600

    Initial commit

---
+++ src/com/company/Boss.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Boss extends GameEntity {
+
+    Weapon weapon = new Weapon();
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        String weapon1 = weapon;
+    }
+
+    public void printInfo(){}
+}
---
+++ src/com/company/Skeleton.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Skeleton extends Boss{
+
+    private int QuantityOfArrows;
+
+    public void setQuantityOfArrows(int numberOfArrows) {
+        this.QuantityOfArrows = numberOfArrows;
+    }
+
+    public int getQuantityOfArrows() {
+        return QuantityOfArrows;
+    }
+}
